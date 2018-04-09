2e3ac5a5b7bdbb7a31e7bdda858dbf5df7bcfbfe
Michael Rojas <michaelrojasp@gmail.com>
2018-04-08 23:57:59 -0600

    Create component function to filter data when the dropdown changes

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { startOfYesterday, endOfYesterday } from 'date-fns';
 
 import News from '../../model/news';
 import { NewsService } from '../../services/news.service';
@@ -28,17 +29,16 @@ export class NewsListComponent implements OnInit {
   getNewsList() {
     this.newsService.getNewsList()
       .subscribe(newsList => {
-        const id = this.route.snapshot.paramMap.get('id');
-        
-        // When the list is rendered for the first time first element in the list is selected
-        if (!id && newsList.length) {
-          const firstId = newsList[0].identifier;
-          this.selectNewsDetails(firstId);
-          this.selectNewsId(firstId);
-        } else {
-          this.selectNewsId(id);
-        }
         this.newsList = newsList;
+        this.checkForDefaultNews();
+      });
+  }
+
+  getNewsListFiltered(from: Date, to: Date) {
+    this.newsService.getNewsListByRange(from, to)
+      .subscribe(newsList => {
+        this.newsList = newsList;
+        this.checkForDefaultNews();
       });
   }
 
@@ -59,7 +59,30 @@ export class NewsListComponent implements OnInit {
   }
 
   changeFilter(filter) {
-    this.getNewsList();
+
+    // TODO: Set the date ranges to call the service and filter the data
+    switch (filter) {
+      case 'yesterday': 
+        const from: Date = startOfYesterday();
+        const to: Date = endOfYesterday();
+        // this.getNewsListFiltered(from, to);
+    }
+  }
+
+  /**
+   * Check if no news id is present in the URL params, if no then select the first element
+   */
+  private checkForDefaultNews() {
+      const id = this.route.snapshot.paramMap.get('id');
+      
+      // When the list is rendered for the first time first element in the list is selected
+      if (!id && this.newsList.length) {
+        const firstId = this.newsList[0].identifier;
+        this.selectNewsDetails(firstId);
+        this.selectNewsId(firstId);
+      } else {
+        this.selectNewsId(id);
+      }
   }
 
 }
